07903593cf68bab738322017ea8f2e0cfcae0314
Egor <raid4k@gmail.com>
2017-11-15 12:46:13 +0300

    mcmillan

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+	$('.wrap-info-book-js').backgroundBlur({
+	    imageURL : 'img/book-cover.jpg',
+	    blurAmount : 100,
+	    endOpacity : 1,
+	    imageClass : 'bg-blur'
+	});
+})
